cb37ce0692927135c74b732f83991a7233bf4dcc
denchik3192 <den465341@gmail.com>
2024-05-24 15:02:27 +0300

    refactor: change title color

--- src/components/MainBunner/LeftBunner.tsx
+++ src/components/MainBunner/LeftBunner.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const LeftBunner: FC = () => {
   return (
     <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-40">
-      <h1 className="mb-4 text-4xl font-bald text-black sm:text-5xl md:mb-8 md:text-6xl">
+      <h1 className="mb-4 text-4xl font-bald text-lime-900 sm:text-5xl md:mb-8 md:text-6xl">
         ДАРЫ ПРИРОДЫ
       </h1>
       <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
